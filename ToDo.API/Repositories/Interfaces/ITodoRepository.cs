f96d9059a85f7c47e078ab31b88315523ec1b653
dabananda <dabananda.dev@gmail.com>
2025-08-13 22:28:39 +0600

    refactor: null handling

--- ToDo.API/Repositories/Interfaces/ITodoRepository.cs
+++ ToDo.API/Repositories/Interfaces/ITodoRepository.cs
@@ -5,7 +5,7 @@ namespace ToDo.API.Repositories.Interfaces
     public interface ITodoRepository
     {
         Task<IEnumerable<ToDoItem>> GetAllAsync();
-        Task<ToDoItem> GetByIdAsync(Guid id);
+        Task<ToDoItem?> GetByIdAsync(Guid id);
         Task<ToDoItem> CreateAsync(ToDoItem todoItem);
         Task<ToDoItem> UpdateAsync(ToDoItem todoItem);
         Task<bool> DeleteAsync(Guid id);
