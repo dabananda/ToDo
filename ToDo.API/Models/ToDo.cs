b05423dfef4ab21c3621dc2aa30b5c88beaad36f
dabananda <dabananda.dev@gmail.com>
2025-08-13 10:07:39 +0600

    feat: todo model

---
+++ ToDo.API/Models/ToDo.cs
@@ -0,0 +1,13 @@
+﻿namespace ToDo.API.Models
+{
+    public class ToDo
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string? Description { get; set; }
+        public bool IsCompleted { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? UpdatedAt { get; set; }
+        public DateTime? CompletedAt { get; set; }
+    }
+}
